f94afe341bd84906ca39a6dc0c395f429fd27efa
Alexander Morozov <sarmirim@gmail.com>
2020-11-19 19:31:59 +0300

    many fixes for react hooks

--- components/reddit.js
+++ components/reddit.js
@@ -1,101 +1,102 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import {StyleSheet, ScrollView, View, Text, FlatList, Image, ActivityIndicator,
-        unstable_enableLogBox, Button, TouchableOpacity, RefreshControl, Share } from 'react-native';
+        Button, TouchableOpacity, RefreshControl, Share } from 'react-native';
 import { PostComponent } from './main/PostComponent'
 import {RHeader} from './header/Header'
 import {CurrentTheme} from './colorScheme'
+// import {createStore} from 'redux'
 
 // TODO optimize useStates 
 export let Mlist = () => {
     // states
-    const [isRefreshing, setRefreshing] = useState(true);
-    const [isLoading, setLoading] = useState(true);
+    const [isRefreshing, setRefreshing] = useState(null);
+    const [isLoading, setLoading] = useState(null);
+
     // reddit json api
     const [data, setData] = useState([]);
     // const [text, setText] = useState(``);
+    
     // url variables
-    const [limit, setLimit] = useState(5);
-    const [sort, setSort] = useState('hot');
-    const [subreddit, setSubreddit] = useState('all');
-    const [sortTime, setSortTime] = useState(``); // t=all&
+    const limit = 7;
+    const sort = 'top'
+    const subreddit = 'all'
+    const sortTime = `t=all&` // all time: t=all&     this year: t=year&     this month: t=month&     this week: t=week&     this day: t=day&     now: t=hour&
     const [link, setLink] = useState(`https://www.reddit.com/r/${subreddit}/${sort}.json?limit=${limit}&raw_json=1`)
-    // else
-    const [momentum, setMomentum] = useState(true);
+
+    // FlatList
+    // const [momentum, setMomentum] = useState(true);
 
     // etc
-    let startLink = `https://www.reddit.com/r/${subreddit}/${sort}.json?limit=${limit}&${sortTime}raw_json=1`
-    let newlink = '';
+    const startLink = `https://www.reddit.com/r/${subreddit}/${sort}.json?limit=${limit}&${sortTime}raw_json=1`
+    let newlink 
 
     useEffect(() => {
-      fetch(startLink)
-        .then((response) => response.json())
-        .then((json) => {
-            setData(json.data.children);
-            console.log("JSON DATA: " + json.data.children);
-            setLink(startLink + "&after=" + json.data.after);
-            console.log(link);
-            newlink = startLink + "&after=" + json.data.after;
-            console.log("newlink" + newlink);
-            console.log("children length: " + json.data.children.length);
-        })
-        .catch((error) => console.error(error))
-        .finally(() => {
-            setRefreshing(false);
-            setLoading(false);
-        });
+        console.log("firstFetch")
+        fetch(startLink)
+            .then((response) => response.json())
+            .then((json) => {
+                setData(json.data.children)
+                setLink(startLink + "&after=" + json.data.after)
+                newlink = startLink + "&after=" + json.data.after
+                console.log("JSON DATA: " + json.data.children)
+                console.log("new "+  json.data.after)
+                console.log("children length: " + json.data.children.length)
+            })
+            .catch((error) => console.error(error))
+            .finally(() => {
+                setRefreshing(false)
+                setLoading(false)
+            });
+        return () => {
+            setData([])
+        }
     }, []);
 
-    let onRefresh = useCallback(() => {
+    let onRefresh = () => {
+        console.log("onRefresh")
         setRefreshing(true)
+        // setData([]);
         fetch(startLink)
         .then((response) => response.json())
         .then((json) => {
             setData(json.data.children);
-            console.log("JSON DATA: " + json.data.children);
-            setLink(startLink + "&after=" + json.data.after);
-            console.log(link);
+            setLink(startLink + "&after=" + json.data.after)
+            console.log("JSON DATA: " + json.data.children)
+            console.log("new "+  json.data.after)
+            console.log("children length: " + json.data.children.length)
             newlink = startLink + "&after=" + json.data.after;
-            console.log("children length: " + json.data.children.length);
         })
         .catch((error) => console.error(error))
         .finally(() => {
+            console.log("finally");
             setRefreshing(false);
             setLoading(false);
         });
-    }, [data]);
+    }
 
-    let _onMomentumScrollBegin = () => {setMomentum(false);
-    console.log("scroll");}
+    // let _onMomentumScrollBegin = () => {
+    //     setMomentum(false);
+    //     console.log("scroll");
+    // }
     
-    // TODO fetch || axios || ??
+    // which one (fetch || axios || ??)
     let loadMore = () => {
+        console.log("loadMore")
         // setLoading(true);
-        fetch(link
-        //     ,{
-        //     method: 'GET',
-        //     headers: {
-        //       'Content-Type': 'application/json'
-        //       // 'Content-Type': 'application/x-www-form-urlencoded',
-        //     },
-        //     redirect: 'follow', // manual, *follow, error
-        //     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        //     body: JSON.stringify(data) // body data type must match "Content-Type" header
-        //   }
-          )
+        fetch(link)
         .then((response) => response.json())
         .then((json) => {
-            setData(data.concat(json.data.children));
-            console.log("JSON DATA: " + json.data.children);
-            setLink(startLink + "&after=" + json.data.after);
-            newlink = startLink + "&after=" + json.data.after;
-            console.log("load more" + newlink);
-            console.log("new "+  json.data.after);
-            console.log("children length: " + json.data.children.length);
+            setData(data.concat(json.data.children))
+            setLink(startLink + "&after=" + json.data.after)
+            newlink = startLink + "&after=" + json.data.after
+            console.log("JSON DATA: " + json.data.children)
+            console.log("new "+  json.data.after)
+            console.log("children length: " + json.data.children.length)
         })
         .catch((error) => console.error(error))
         .finally(() => {
-            setLoading(false);
-        });
+            setLoading(false)
+        })
     }
 
     let renderHeader = () => {
@@ -103,7 +104,7 @@ export let Mlist = () => {
     };
 
     let renderSeparator = () => {
-        return (<View style={{height: 7, backgroundColor: "#1a1a1c",}}/>);
+        return <View style={{height: 7, backgroundColor: "#1a1a1c",}}/>;
     };
 
     let renderFooter = () => {
@@ -113,42 +114,41 @@ export let Mlist = () => {
         </View>
     };
 
+    let renderPost = ({item})=>{
+        return <>
+            <PostComponent
+                Subreddit={item.data.subreddit}
+                PostDetails=''
+                ContentText={item.data.title}
+                UpvotesText={item.data.ups}
+                CommentText={item.data.num_comments}
+                Author={item.data.author} 
+                url={item.data.url}
+                created_utc={item.data.created_utc}
+                over_18={item.data.over_18}
+                permalink={item.data.permalink}
+            >
+            </PostComponent>
+        </>
+    }
     return (
         <FlatList
             data={data}
-            keyExtractor={({ data }, index) => data.id}
+            keyExtractor={({ data }, index) => data.id+index} // added (+index) for unique key
             refreshControl={
                 <RefreshControl
                 refreshing={isRefreshing}
-                onRefresh={()=>{onRefresh()}}
+                onRefresh={() => onRefresh()}
                 />
             }
             ItemSeparatorComponent={renderSeparator}
             ListHeaderComponent={renderHeader}
             ListFooterComponent={renderFooter}
-            renderItem={({ item }) => (
-                <View style={styles.container}>
-                    {/* {console.log(item.data.media.reddit_video.fallback_url)} */}
-                    <PostComponent
-                        Subreddit={item.data.subreddit}
-                        PostDetails=''
-                        ContentText={item.data.title}
-                        UpvotesText={item.data.ups}
-                        CommentText={item.data.num_comments}
-                        Author={item.data.author}
-                        style={styles.postComponent}
-                        url={item.data.url}
-                        created_utc={item.data.created_utc}
-                        over_18={item.data.over_18}
-                        permalink={item.data.permalink}
-                    >
-                    </PostComponent>
-                </View>
-            )}                    
+            renderItem={renderPost}                    
                 // showsVerticalScrollIndicator={false}
-                onEndReached={() => {console.log("finish"); loadMore()}}
+                onEndReached={loadMore}
                 onEndReachedThreshold={0.1}
-                onMomentumScrollBegin={() => _onMomentumScrollBegin()}
+                // onMomentumScrollBegin={() => _onMomentumScrollBegin()}
         />
     )
 }
